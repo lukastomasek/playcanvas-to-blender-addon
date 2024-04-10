fe9f3792ac76d2af81e0e0b7efe9a7bcfa0757b8
Lukas Tomasek <vedstav31@gmail.com>
2024-04-10 11:02:55 -0400

    Loading playcanvas scene

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import time
 from pathlib import Path
 
+# Addon Info
 bl_info = {
   "name": "Playcanvas Scene Convertor",
   "description": "Playcanvas to Blender Addon for recreating scenes and high quality rendering",
@@ -18,24 +19,35 @@ bl_info = {
 
 LOCAL_MODEL_URL = ""
 JSON_ID_NAME = "custom.load_json"
-render_progress = 0
 
-def render_with_cycles(scene):
-  bpy.context.scene.render.engine = "CYCLES"
-  output_path = str(Path.home() /  "Downloads")
-  bpy.ops.render.render('INVOKE_DEFAULT', animation=False, write_still=True)
 
-def load_glb_from_url(url=LOCAL_MODEL_URL, is_local_path=False):
-  if is_local_path:
-     bpy.ops.import_scene.gltf(filepath=url)
-  else:
-    filepath = bpy.path.abspath("/Users/lukastomasek/Downloads/temp.glb")
-    urllib.request.urlretrieve(url, filepath)
-    bpy.ops.import_scene.gltf(filepath=filepath, filter_glob='*.glb', import_pack_images=True)
-    os.remove(local_path)
+#####
+# UTILITY CLASSES
+####
 
-# CREATE UI PANEL CLASS 
-class PcToBlenderPanel(bpy.types.Panel):
+class RenderEngine():
+  def render(self):
+    bpy.context.scene.render.engine = "CYCLES"
+    output_path = str(Path.home() /  "Downloads")
+    bpy.ops.render.render('INVOKE_DEFAULT', animation=False, write_still=True)
+
+
+class Loader():
+  def load_glb_from_url(self, url=LOCAL_MODEL_URL, is_local_path=False):
+    if is_local_path:
+       bpy.ops.import_scene.gltf(filepath=url)
+    else:
+      local_path = str(Path.home() /  "Downloads/temp.glb")
+      filepath = bpy.path.abspath(local_path)
+      urllib.request.urlretrieve(url, filepath)
+      bpy.ops.import_scene.gltf(filepath=filepath, filter_glob='*.glb', import_pack_images=True)
+      os.remove(local_path)
+
+#####
+# INTERFACE
+####
+
+class Panel(bpy.types.Panel):
   bl_label = "Playcanvas Scene Convertor"
   bl_idname = "pc_to_blender"
   bl_space_type = "VIEW_3D"
@@ -52,18 +64,20 @@ class PcToBlenderPanel(bpy.types.Panel):
     layout.label(text="Rendering")
     layout.operator("custom.render", text="Render Scene")
 
-    layout.progress(factor=0.1)
 
+#####
+# INTERFACE OPERATORS/CALLBACKS
+####
 
 bpy.types.Scene.json_file_path = bpy.props.StringProperty(
   name="JSON File Path",
   subtype="FILE_PATH",
 )
 
-# Utils class to load JSON
 class LoadJSON(bpy.types.Operator):
   bl_idname = JSON_ID_NAME
   bl_label = "Load JSON"
+  loader = Loader()
 
   def execute(self, context):
     scene = context.scene
@@ -73,30 +87,29 @@ class LoadJSON(bpy.types.Operator):
      with open(file_path) as file:
         data = json.load(file)
         floor_plan_url = data["floor_plan"]
-        load_glb_from_url(floor_plan_url, is_local_path=False)
-
+        self.loader.load_glb_from_url(floor_plan_url, is_local_path=False)
     else:
       self.report({'WARNING'}, "No JSON file selected")
 
     return {'FINISHED'}
 
-
 class RenderScene(bpy.types.Operator):
   bl_idname = "custom.render"
   bl_label = "Render Scene"
+  render_engine = RenderEngine()
 
   def execute(self, context):
-    render_with_cycles(context.scene)
+    self.render_engine.render()
     return {'FINISHED'}
 
 def register():
-  bpy.utils.register_class(PcToBlenderPanel)
+  bpy.utils.register_class(Panel)
   bpy.utils.register_class(LoadJSON)
   bpy.utils.register_class(RenderScene)
 
 
 def unregister():
-  bpy.utils.unregister_class(PcToBlenderPanel)
+  bpy.utils.unregister_class(Panel)
   bpy.utils.unregister_class(LoadJSON)
   bpy.utils.unregister_class(RenderScene)
 
